95d5f84424693d74d0dcbaa5c766913894174eab
Jake J <jakejohnson818@gmail.com>
2018-05-15 14:55:02 -0500

    Put main in a do while

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Scanner scname = new Scanner(System.in);
         Scanner scnum = new Scanner(System.in);
+        int choice = sc.nextInt();
         String directory = "data";
         String fileName = "contacts.txt";
+
+
+
         System.out.println("What would you like to do?");
         System.out.println();
         System.out.println("1. View Contacts");
@@ -23,38 +27,35 @@ public class Main {
         System.out.println("5. Exit");
         System.out.println();
         System.out.print(">");
-        int choice = sc.nextInt();
-        System.out.println();
-        if (choice == 1) {
-            readLines(directory,fileName);
-        }
-        else if (choice == 2) {
-            ArrayList<String> num = new ArrayList<>();
-            String name;
-            String number;
-            System.out.println("What is the contacts name");
-            System.out.print(">");
-            name  = scname.nextLine();
-            System.out.println("What is the contacts number");
-            System.out.print(">");
-            number  = scnum.nextLine();
-            num.add(name+" - "+number);
 
-            addContact(num,directory,fileName);
-            System.out.println("Contact added");
-        }
-        else if (choice == 3) {
+        do {
+            System.out.println();
+            if (choice == 1) {
+                readLines(directory, fileName);
+            } else if (choice == 2) {
+                ArrayList<String> num = new ArrayList<>();
+                String name;
+                String number;
+                System.out.println("What is the contacts name");
+                System.out.print(">");
+                name = scname.nextLine();
+                System.out.println("What is the contacts number");
+                System.out.print(">");
+                number = scnum.nextLine();
+                num.add(name + " - " + number);
 
-        }
-        else if (choice == 4) {
+                addContact(num, directory, fileName);
+                System.out.println("Contact added");
+            } else if (choice == 3) {
 
-        }
-        else if (choice == 5) {
+            } else if (choice == 4) {
 
-        }
-        else {
-            System.out.println("try again fucko");
-        }
+            } else if (choice == 5) {
+
+            } else {
+                System.out.println("try again");
+            }
+        }while(choice != 5);
 
     }
 
